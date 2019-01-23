9e83e030aee455fe51a7287c4caad1a371e4f743
choi <choi@192.168.0.70>
2019-01-23 20:03:47 +0900

    u

--- bookmall/src/com/douzon/bookmall/dao/BookDao.java
+++ bookmall/src/com/douzon/bookmall/dao/BookDao.java
@@ -11,30 +11,30 @@ import java.util.List;
 import com.douzon.bookmall.vo.BookVo;
 import com.douzon.bookmall.vo.CategoryVo;
 
-public class BookDao extends AbstractDao{
+public class BookDao extends AbstractDao {
 	public boolean insert(BookVo bookVo) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		boolean result = false;
 		try {
 			conn = getConnection();
-			String sql ="insert into book values(null,?,?,?)";
+			String sql = "insert into book values(null,?,?,?)";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, bookVo.getName());
 			pstmt.setLong(2, bookVo.getPrice());
 			pstmt.setLong(3, bookVo.getCategory().getNo());
-			
+
 			int count = pstmt.executeUpdate();
 			result = count == 1;
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					conn.close();
 				}
 			} catch (SQLException e) {
@@ -43,42 +43,39 @@ public class BookDao extends AbstractDao{
 		}
 		return result;
 	}
-	
-	
-	public BookVo getBook(long no)
-	{
+
+	public BookVo getBook(long no) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		BookVo vo = new BookVo();
 		try {
 			conn = getConnection();
-			
 
 			String sql = "select * from book where no= ?";
-			pstmt=conn.prepareStatement(sql);
+			pstmt = conn.prepareStatement(sql);
 			pstmt.setLong(1, no);
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				vo.setNo(rs.getLong(1));
 				vo.setName(rs.getString(2));
 				vo.setPrice(rs.getLong(3));
 				vo.setCategory(new CategoryDao().getCategory((rs.getLong(4))));
-				
+
 			}
-			
+
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				if(rs != null) {
+				if (rs != null) {
 					rs.close();
 				}
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					conn.close();
 				}
 			} catch (SQLException e) {
@@ -87,9 +84,8 @@ public class BookDao extends AbstractDao{
 		}
 		return vo;
 	}
-	
-	
-	public List<BookVo> getList(){
+
+	public List<BookVo> getList() {
 		List<BookVo> list = new ArrayList<BookVo>();
 		Connection conn = null;
 		Statement stmt = null;
@@ -100,43 +96,41 @@ public class BookDao extends AbstractDao{
 
 			String sql = "select * from book order by no asc";
 			rs = stmt.executeQuery(sql);
-			
-			while(rs.next()) {
-				long no=rs.getLong(1);
+
+			while (rs.next()) {
+				long no = rs.getLong(1);
 				String name = rs.getString(2);
-				long price=rs.getLong(3);
-				CategoryVo category=new CategoryDao().getCategory(rs.getLong(4));
-				
+				long price = rs.getLong(3);
+				CategoryVo category = new CategoryDao().getCategory(rs.getLong(4));
+
 				BookVo vo = new BookVo();
 				vo.setNo(no);
 				vo.setName(name);
 				vo.setPrice(price);
 				vo.setCategory(category);
-				
+
 				list.add(vo);
 			}
-			
+
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				if(rs != null) {
+				if (rs != null) {
 					rs.close();
 				}
-				if(stmt != null) {
+				if (stmt != null) {
 					stmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		
+
 		return list;
 	}
-	
-	
+
 }
